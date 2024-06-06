d6e0bbe8c2f5d3f75045b4b850d352a3e8ff7277
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2024-06-06 16:15:36 +0800

    root: added automataci ci automation infrasturcture
    Since we want to develop this library to be a working one, we
    should integrate a facility for it. Hence, let's add AutomataCI.
    
    This patch adds AutomataCI CI automation infrastructure into
    root repository.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ srcNODE/.ci/prepare_unix-any.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at:
+#                 http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+
+
+
+# initialize
+if [ "$PROJECT_PATH_ROOT" = "" ]; then
+        >&2 printf "[ ERROR ] - Please run from automataCI/ci.sh.ps1 instead!\n"
+        return 1
+fi
+
+. "${LIBS_AUTOMATACI}/services/i18n/translations.sh"
+. "${LIBS_AUTOMATACI}/services/compilers/node.sh"
+
+
+
+
+# execute
+I18N_Activate_Environment
+NODE_Activate_Local_Environment
+if [ $? -ne 0 ]; then
+        I18N_Activate_Failed
+        return 1
+fi
+
+
+I18N_Import_Dependencies
+__current_path="$PWD" && cd "${PROJECT_PATH_ROOT}/${PROJECT_NODE}"
+NODE_NPM_Install_Dependencies_All
+___process=$?
+cd "$__current_path" && unset __current_path
+if [ $___process -ne 0 ]; then
+        I18N_Import_Failed
+        return 1
+fi
+
+
+
+
+# report status
+return 0
