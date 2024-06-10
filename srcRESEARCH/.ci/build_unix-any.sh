77e963f5a2b77a5c25dbe40ba89cfc4a9cce35a0
(Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
2024-06-10 08:58:01 +0800

    root: enabled research paper pipeline (srcRESEARCH)
    Since we want this library to be reliably good, we should enable
    the research paper pipeline and place all our details and data in
    it. That way, interested folks and code auditors can review the
    materials without needing to ping-pong with the maintainers that
    often.
    
    This patch enables research paper pipeline (srcRESEARCH) in
    root repository.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <me@hollowaykeanho.com>

---
+++ srcRESEARCH/.ci/build_unix-any.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at:
+#                 http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+
+
+
+# initialize
+if [ "$PROJECT_PATH_ROOT" = "" ]; then
+        >&2 printf "[ ERROR ] - Please run from automataCI/ci.sh.ps1 instead!\n"
+        return 1
+fi
+
+. "${LIBS_AUTOMATACI}/services/io/fs.sh"
+. "${LIBS_AUTOMATACI}/services/i18n/translations.sh"
+. "${LIBS_AUTOMATACI}/services/compilers/libreoffice.sh"
+
+
+
+
+# execute
+I18N_Activate_Environment
+LIBREOFFICE_Is_Available
+if [ $? -ne 0 ]; then
+        I18N_Activate_Failed
+        return 1
+fi
+
+
+
+
+# setup inportant variables
+___name="${PROJECT_SKU}-${PROJECT_RESEARCH_IDENTIFIER}_${PROJECT_VERSION}_any-any"
+___source="research-paper.odt"
+
+
+
+
+# build PDF
+___source="${PROJECT_PATH_ROOT}/${PROJECT_RESEARCH}/${___source}"
+___dest="${PROJECT_PATH_ROOT}/${PROJECT_PATH_TEMP}/build-${___name}"
+I18N_Prepare "$___source"
+FS_Is_File "$___source"
+if [ $? -ne 0 ]; then
+        I18N_Prepare_Failed
+        return 1
+fi
+
+FS_Remake_Directory "$___dest"
+
+## IMPORTANT: refer the following page for modifying parameters:
+##   https://help.libreoffice.org/latest/en-US/text/shared/guide/pdf_params.html
+I18N_Build "$___source"
+$(LIBREOFFICE_Get) --headless --convert-to "pdf:writer_pdf_Export:{
+        \"UseLosslessCompression\": true,
+        \"Quality\": 100,
+        \"SelectPdfVersion\": 0,
+        \"PDFUACompliance\": false,
+        \"UseTaggedPDF\": true,
+        \"ExportFormFields\": true,
+        \"FormsType\": 1,
+        \"ExportBookmarks\": true,
+        \"ExportPlaceholders\": true
+}" --outdir "$___dest" "$___source"
+___process=$?
+if [ $___process -ne 0 ]; then
+        I18N_Build_Failed
+        return 1
+fi
+
+
+
+
+## export output
+___source="${___dest}/$(FS_Get_File "$___source")"
+___source="$(FS_Extension_Replace "$___source" ".odt" ".pdf")"
+___dest="${PROJECT_PATH_ROOT}/${PROJECT_PATH_BUILD}/${___name}.pdf"
+
+FS_Is_File "$___source"
+if [ $? -ne 0 ]; then
+        I18N_Build_Failed
+        return 1
+fi
+
+I18N_Export "$___dest"
+FS_Remove_Silently "$___dest"
+FS_Copy_File "$___source" "$___dest"
+if [ $? -ne 0 ]; then
+        I18N_Export_Failed
+        return 1
+fi
+
+
+
+
+# report status
+return 0
---
+++ srcRESEARCH/.ci/build_windows-any.ps1
@@ -0,0 +1,105 @@
+# Copyright 2024 (Holloway) Chew, Kean Ho <hello@hollowaykeanho.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at:
+#                 http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+
+
+# initialize
+if (-not (Test-Path -Path $env:PROJECT_PATH_ROOT)) {
+	Write-Error "[ ERROR ] - Please run from automataCI\ci.sh.ps1 instead!`n"
+	return 1
+}
+
+. "${env:LIBS_AUTOMATACI}\services\io\fs.ps1"
+. "${env:LIBS_AUTOMATACI}\services\i18n\translations.ps1"
+. "${env:LIBS_AUTOMATACI}\services\compilers\libreoffice.ps1"
+
+
+
+
+# execute
+$null = I18N-Activate-Environment
+$___process = LIBREOFFICE-Is-Available
+if ($___process -ne 0) {
+	$null = I18N-Activate-Failed
+	return 1
+}
+
+
+
+
+# setup important variables
+$___name = "${env:PROJECT_SKU}-${env:PROJECT_RESEARCH_IDENTIFIER}_${env:PROJECT_VERSION}_any-any"
+$___source = "research-paper.odt"
+
+
+
+
+# build PDF
+$___source = "${env:PROJECT_PATH_ROOT}\${env:PROJECT_RESEARCH}\${___source}"
+$___dest = "${env:PROJECT_PATH_ROOT}\${env:PROJECT_PATH_TEMP}\build-${___name}"
+$null = I18N-Prepare "${___source}"
+$___process = FS-Is-File "${___source}"
+if ($___process -ne 0) {
+	$null = I18N-Prepare-Failed
+	return 1
+}
+$null = FS-Remake-Directory "${___dest}"
+
+## IMPORTANT: refer the following page for modifying parameters:
+##   https://help.libreoffice.org/latest/en-US/text/shared/guide/pdf_params.html
+$null = I18N-Build "${___source}"
+$___process = OS-Exec "$(LIBREOFFICE-Get)" @"
+--headless --convert-to "pdf:writer_pdf_Export:{
+	"UseLosslessCompression": true,
+	"Quality": 100,
+	"SelectPdfVersion": 0,
+	"PDFUACompliance": false,
+	"UseTaggedPDF": true,
+	"ExportFormFields": true,
+	"FormsType": 1,
+	"ExportBookmarks": true,
+	"ExportPlaceholders": true,
+}" --outdir "${___dest}" "${___source}"
+"@
+if ($___process -ne 0) {
+	$null = I18N-Build-Failed
+	return 1
+}
+
+
+
+
+# export output
+$___source = "${___dest}\$(FS-Get-File "${___source}")"
+$___source = FS-Extension-Replace "${___source}" ".odt" ".pdf"
+$___dest = "${env:PROJECT_PATH_ROOT}\${env:PROJECT_PATH_BUILD}\${___name}.pdf"
+
+$___process = FS-Is-File "${___source}"
+if ($___process -ne 0) {
+	$null = I18N-Build-Failed
+	return 1
+}
+
+$null = I18N-Export "${___dest}"
+$null = FS-Remove-Silently "${___dest}"
+$___process = FS-Copy-File "${___source}" "${___dest}"
+if ($___process -ne 0) {
+	$null = I18N-Export-Failed
+	return 1
+}
+
+
+
+
+# report status
+return 0
